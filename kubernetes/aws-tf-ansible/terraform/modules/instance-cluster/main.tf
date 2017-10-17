6f6d4220f456c40f53ab3cf77aae2fe17b1ff143
Scott Lowe <scott.lowe@scottlowe.org>
2017-10-17 15:41:23 -0600

    Start modularizing Terraform code
    Start the process of modularizing the Terraform code. Standardize filenames to be more consistent.
    
    Signed-off-by: Scott Lowe <scott.lowe@scottlowe.org>

---
+++ kubernetes/aws-tf-ansible/terraform/modules/instance-cluster/main.tf
@@ -0,0 +1,28 @@
+# Launch a cluster of instances
+resource "aws_instance" "instance" {
+    count = "${var.cluster_size}"
+    ami = "${var.ami}"
+    instance_type = "${var.type}"
+    subnet_id = "${element(var.subnet_list, count.index)}"
+    key_name = "${var.ssh_key}"
+    associate_public_ip_address = "${var.assign_pub_ip}"
+    vpc_security_group_ids = ["${var.sec_group_list}"]
+
+    tags {
+        Name = "${var.name}-${count.index}"
+        Role = "${var.role}"
+    }
+}
+
+# Provide some outputs to the code that called the module
+output "public_ip_list" {
+    value = "${join(",", aws_instance.instance.*.public_ip)}"
+}
+
+output "private_ip_list" {
+    value = "${join(",", aws_instance.instance.*.private_ip)}"
+}
+
+output "instance_id_list" {
+    value = "${join(",", aws_instance.instance.*.id)}"
+}
---
+++ kubernetes/aws-tf-ansible/terraform/modules/vpc/main.tf
@@ -0,0 +1,69 @@
+# Create a new VPC
+resource "aws_vpc" "vpc" {
+    cidr_block              = "${var.vpc_cidr_block}"
+    enable_dns_hostnames    = "${var.vpc_dns_hostnames}"
+    enable_dns_support      = "${var.vpc_dns_support}"
+
+    tags {
+        Name                = "${var.name}_vpc"
+        tool                = "terraform"
+    }
+}
+
+# Modify default VPC security group to allow inbound SSH
+resource "aws_security_group_rule" "allow_ssh" {
+    type                    = "ingress"
+    from_port               = 22
+    to_port                 = 22
+    protocol                = "tcp"
+    cidr_blocks             = ["0.0.0.0/0"]
+    security_group_id       = "${aws_vpc.vpc.default_security_group_id}"
+}
+
+# Create a new Internet gateway
+resource "aws_internet_gateway" "gateway" {
+    vpc_id                  = "${aws_vpc.vpc.id}"
+
+    tags {
+        Name                = "${var.name}_igw"
+        tool                = "terraform"
+    }
+}
+
+# Add default route to VPC's main route table
+resource "aws_route" "default_route" {
+    route_table_id          = "${aws_vpc.vpc.main_route_table_id}"
+    destination_cidr_block  = "0.0.0.0/0"
+    gateway_id              = "${aws_internet_gateway.gateway.id}"
+}
+
+# Create a public subnet in the new VPC
+resource "aws_subnet" "subnet" {
+    count                   = "${length(data.aws_availability_zones.az_list.names)}"
+    vpc_id                  = "${aws_vpc.vpc.id}"
+    cidr_block              = "${cidrsubnet(var.vpc_cidr_block, 4, count.index)}"
+    availability_zone       = "${element(data.aws_availability_zones.az_list.names, count.index)}"
+    map_public_ip_on_launch = "${var.subnet_map_pub_ip}"
+
+    tags {
+        Name                = "${var.name}_subnet_${count.index}"
+        tool                = "terraform"
+    }
+}
+
+# Provide some output to the code that called the module
+output "vpc_id" {
+    value                   = "${aws_vpc.vpc.id}"
+}
+
+output "subnet_id" {
+    value                   = "${aws_subnet.subnet.*.id}"
+}
+
+output "subnet_az" {
+    value                   = "${aws_subnet.subnet.*.availability_zone}"
+}
+
+output "default_sg_id" {
+    value                   = "${aws_vpc.vpc.default_security_group_id}"
+}
