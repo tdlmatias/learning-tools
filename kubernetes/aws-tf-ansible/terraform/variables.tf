3f81bed136b9f0cc3c3b1a416999fadf66319b70
Scott Lowe <scott.lowe@scottlowe.org>
2017-10-17 15:41:23 -0600

    Start modularizing Terraform code
    Start the process of modularizing the Terraform code. Standardize filenames to be more consistent.
    
    Signed-off-by: Scott Lowe <scott.lowe@scottlowe.org>

--- kubernetes/aws-tf-ansible/terraform/variables.tf
+++ kubernetes/aws-tf-ansible/terraform/variables.tf
@@ -1,49 +1,41 @@
 variable "keypair" {
     type                    = "string"
     description             = "SSH keypair to use to connect to instances"
-    default                 = "itx_rsa"
 }
 
-variable "etcd_flavor" {
+variable "etcd_size" {
     type                    = "string"
     description             = "AWS type to use for etcd instances"
-    default                 = "t2.small"
 }
 
 variable "etcd_count" {
     type                    = "string"
     description             = "Number of etcd instances to create"
-    default                 = "3"
 }
 
-variable "controller_flavor" {
+variable "controller_size" {
     type                    = "string"
     description             = "AWS type to use for K8s controller instances"
-    default                 = "t2.small"
 }
 
 variable "controller_count" {
     type                    = "string"
     description             = "Number of K8s controller instances to create"
-    default                 = "3"
 }
 
-variable "worker_flavor" {
+variable "worker_size" {
     type                    = "string"
     description             = "AWS type to use for K8s worker instances"
-    default                 = "t2.micro"
 }
 
 variable "worker_count" {
     type                    = "string"
     description             = "Number of K8s worker instances to create"
-    default                 = "3"
 }
 
 variable "user_region" {
     type                    = "string"
     description             = "AWS region to use by default"
-    default                 = "us-west-2"
 }
 
 variable "kubernetes_cluster_dns" {
